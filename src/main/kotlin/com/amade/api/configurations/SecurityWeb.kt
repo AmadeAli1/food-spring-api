3a02ea15e8b6e984204e9479e90064af74573f7d
AmadeAli1 <aliamade29@gmail.com>
2022-06-05 04:25:36 +0200

    UsuarioController.kt

--- src/main/kotlin/com/amade/api/configurations/SecurityWeb.kt
+++ src/main/kotlin/com/amade/api/configurations/SecurityWeb.kt
@@ -3,22 +3,32 @@ package com.amade.api.configurations
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.server.SecurityWebFilterChain
+
 
 @Configuration
 @EnableWebFluxSecurity
 class SecurityWeb {
 
     @Bean
-    fun userDetailsService(): ReactiveUserDetailsService {
-        val userDetails = User.withDefaultPasswordEncoder()
-            .username("Amade")
-            .password("amade-2022")
-            .roles("USER", "ADMIN")
+    fun userDetailsService(passwordEncoder: PasswordEncoder): MapReactiveUserDetailsService? {
+        val user = User
+            .withUsername("amade")
+            .password(passwordEncoder.encode("amade-2022"))
+            .roles("USER")
             .build()
-        return MapReactiveUserDetailsService(userDetails)
+        return MapReactiveUserDetailsService(user)
+    }
+
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        return http.authorizeExchange()
+            .anyExchange().authenticated()
+            .and().build()
     }
 
 }
