a6327aac05e06543e5b19910e83c66eaa16f01fb
AmadeAli1 <aliamade29@gmail.com>
2022-06-06 02:02:12 +0200

    WebSecurity

--- src/main/kotlin/com/amade/api/model/Usuario.kt
+++ src/main/kotlin/com/amade/api/model/Usuario.kt
@@ -6,16 +6,13 @@ import org.springframework.data.relational.core.mapping.Table
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import java.util.*
-import javax.validation.constraints.Email
-import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
-import javax.validation.constraints.Size
+import javax.validation.constraints.*
 
 @Table("usuario")
 data class Usuario(
     @field:Id @field:Column("uid") val uid: String = UUID.randomUUID().toString(),
     @field:Size(max = 40, min = 2) @field:NotNull @field:Column("username") val name: String,
-    @field:NotNull @field:Column("password") val senha: String,
+    @field:NotNull @field:NotBlank @field:Min(value = 6) @field:Column("password") val senha: String,
     @field:Email @field:NotBlank @field:Column("email") val email: String,
     @field:NotNull @field:Column("role") val role: UsuarioRole = UsuarioRole.USER,
 ) : UserDetails {
@@ -24,11 +21,9 @@ data class Usuario(
         val authority = SimpleGrantedAuthority(role.name)
         return listOf(authority)
     }
-
     override fun getPassword(): String {
         return senha
     }
-
     override fun getUsername(): String {
         return email
     }
