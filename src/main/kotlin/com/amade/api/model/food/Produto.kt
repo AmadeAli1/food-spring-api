ece66775eff5e0a9436bed69fff73f342978a7cd
AmadeAli1 <aliamade29@gmail.com>
2022-06-14 00:45:16 +0200

    Produto.kt change LocalTime to LocalDateTime

--- src/main/kotlin/com/amade/api/model/food/Produto.kt
+++ src/main/kotlin/com/amade/api/model/food/Produto.kt
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.sql.Date
-import java.time.LocalDate
+import java.time.LocalDateTime
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
@@ -18,11 +18,11 @@ data class Produto(
     @field:NotNull @field:Min(1) @Column("quantidade") var quantity: Int,
     @field:NotNull @Column("category_id") var category: Int,
     @Column("likes") var likes: Int = 0,
-    @Column("createdAt") var date: LocalDate? = Date(System.currentTimeMillis()).toLocalDate(),
+    @Column("createdAt") var date: LocalDateTime? = LocalDateTime.now(),
 ) {
     @Id
     @Column("id")
     var id: Int = 0
 
-    constructor():this("",0.0f,0,0,0,Date(System.currentTimeMillis()).toLocalDate())
+    constructor() : this("", 0.0f, 0, 0, 0, LocalDateTime.now())
 }
