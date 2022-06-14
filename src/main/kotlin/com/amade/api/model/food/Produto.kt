161348366ee5f2be0ec434d40abe5898952b56b3
AmadeAli1 <aliamade29@gmail.com>
2022-06-14 09:10:29 +0200

    Produto.kt change String to LocalDateTime and add field="description in Producto Table"

--- src/main/kotlin/com/amade/api/model/food/Produto.kt
+++ src/main/kotlin/com/amade/api/model/food/Produto.kt
@@ -1,15 +1,15 @@
 package com.amade.api.model.food
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.hibernate.validator.constraints.Length
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
-import java.sql.Date
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
-
 
 @Table("produto")
 data class Produto(
@@ -18,11 +18,20 @@ data class Produto(
     @field:NotNull @field:Min(1) @Column("quantidade") var quantity: Int,
     @field:NotNull @Column("category_id") var category: Int,
     @Column("likes") var likes: Int = 0,
-    @Column("createdAt") var date: String? = LocalDateTime.now().toString(),
+    @field:NotNull @field:NotBlank @field:Length(max = 255, min = 10) @Column("description") var description: String,
+    @field:JsonIgnore
+    @Column("createdAt") var date: LocalDateTime? = LocalDateTime.now(),
 ) {
     @Id
     @Column("id")
     var id: Int = 0
 
-    constructor() : this("", 0.0f, 0, 0, 0, LocalDateTime.now().toString())
+    @org.springframework.data.annotation.Transient
+    var createdAt: String? = date!!.format(DateTimeFormatter.ISO_DATE_TIME)
+
+    constructor() : this(
+        "", 0.0f, 0,
+        0, 0, "", LocalDateTime.now()
+    )
+
 }
